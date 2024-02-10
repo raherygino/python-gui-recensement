a8412594c42256120b580650f3c85329a44b48f1
raherygino <georgino197@gmail.com>
2024-02-10 21:21:16 +0100

    Init qfluent library

--- main.py
+++ main.py
@@ -1,105 +1,42 @@
+# coding:utf-8
+import os
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget, QLabel, QLineEdit, QPushButton, QTableWidget, QTableWidgetItem
-from PyQt5.QtCore import Qt
-import sqlite3
 
-class Model:
-    def __init__(self):
-        self.conn = sqlite3.connect("database.db")
-        self.create_table()
+from PyQt5.QtCore import Qt, QTranslator
+from PyQt5.QtGui import QFont
+from PyQt5.QtWidgets import QApplication
+from qfluentwidgets import FluentTranslator
 
-    def create_table(self):
-        cursor = self.conn.cursor()
-        cursor.execute('''CREATE TABLE IF NOT EXISTS items (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT)''')
-        self.conn.commit()
+from app.common.config import cfg
+from app.view.main_window import MainWindow
 
-    def fetch_all_items(self):
-        cursor = self.conn.cursor()
-        cursor.execute('''SELECT * FROM items''')
-        return cursor.fetchall()
 
-    def add_item(self, name):
-        cursor = self.conn.cursor()
-        cursor.execute('''INSERT INTO items (name) VALUES (?)''', (name,))
-        self.conn.commit()
+# enable dpi scale
+if cfg.get(cfg.dpiScale) == "Auto":
+    QApplication.setHighDpiScaleFactorRoundingPolicy(
+        Qt.HighDpiScaleFactorRoundingPolicy.PassThrough)
+    QApplication.setAttribute(Qt.AA_EnableHighDpiScaling)
+else:
+    os.environ["QT_ENABLE_HIGHDPI_SCALING"] = "0"
+    os.environ["QT_SCALE_FACTOR"] = str(cfg.get(cfg.dpiScale))
 
-    def update_item(self, item_id, new_name):
-        cursor = self.conn.cursor()
-        cursor.execute('''UPDATE items SET name=? WHERE id=?''', (new_name, item_id))
-        self.conn.commit()
+QApplication.setAttribute(Qt.AA_UseHighDpiPixmaps)
 
-    def delete_item(self, item_id):
-        cursor = self.conn.cursor()
-        cursor.execute('''DELETE FROM items WHERE id=?''', (item_id,))
-        self.conn.commit()
+# create application
+app = QApplication(sys.argv)
+app.setAttribute(Qt.AA_DontCreateNativeWidgetSiblings)
 
-class Presenter:
-    def __init__(self, view, model):
-        self.view = view
-        self.model = model
-        self.populate_table()
-        self.view.show()
+# internationalization
+locale = cfg.get(cfg.language).value
+translator = FluentTranslator(locale)
+galleryTranslator = QTranslator()
+galleryTranslator.load(locale, "gallery", ".", ":/gallery/i18n")
 
-    def populate_table(self):
-        items = self.model.fetch_all_items()
-        self.view.populate_table(items)
+app.installTranslator(translator)
+app.installTranslator(galleryTranslator)
 
-    def add_item(self, name):
-        self.model.add_item(name)
-        self.populate_table()
+# create main window
+w = MainWindow()
+w.show()
 
-    def update_item(self, item_id, new_name):
-        self.model.update_item(item_id, new_name)
-        self.populate_table()
-
-    def delete_item(self, item_id):
-        self.model.delete_item(item_id)
-        self.populate_table()
-
-class View(QMainWindow):
-    def __init__(self):
-        super().__init__()
-
-        self.central_widget = QWidget()
-        self.setCentralWidget(self.central_widget)
-
-        self.layout = QVBoxLayout()
-
-        self.name_label = QLabel("Name:")
-        self.name_edit = QLineEdit()
-        self.add_button = QPushButton("Add")
-        self.add_button.clicked.connect(self.add_clicked)
-
-        self.table = QTableWidget()
-        self.table.setColumnCount(2)
-        self.table.setHorizontalHeaderLabels(["ID", "Name"])
-
-        self.layout.addWidget(self.name_label)
-        self.layout.addWidget(self.name_edit)
-        self.layout.addWidget(self.add_button)
-        self.layout.addWidget(self.table)
-
-        self.central_widget.setLayout(self.layout)
-
-    def add_clicked(self):
-        name = self.name_edit.text()
-        self.presenter.add_item(name)
-        self.name_edit.clear()
-
-    def populate_table(self, items):
-        self.table.setRowCount(0)
-        for row, item in enumerate(items):
-            self.table.insertRow(row)
-            for col, value in enumerate(item):
-                self.table.setItem(row, col, QTableWidgetItem(str(value)))
-
-def main():
-    app = QApplication(sys.argv)
-    model = Model()
-    view = View()
-    presenter = Presenter(view, model)
-    view.presenter = presenter
-    sys.exit(app.exec_())
-
-if __name__ == '__main__':
-    main()
+app.exec_()
