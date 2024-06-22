aab115f25498d9ee8959b0e32278087fc83b4dcc
raherygino <georgino197@gmail.com>
2024-06-22 09:20:11 +0300

    Update components

--- app/components/dialog_import.py
+++ app/components/dialog_import.py
@@ -3,17 +3,18 @@ from PyQt5.QtCore import Qt
 from qfluentwidgets import MessageBoxBase, SubtitleLabel, LineEdit, ComboBox, BodyLabel
 
 class DialogImport(MessageBoxBase):
-    def __init__(self, firstData:list, columns:list, parent=None):
+    def __init__(self, data:list, columns:list, parent=None):
         super().__init__(parent)
         self.viewLayout.addWidget(SubtitleLabel('Importer', self))
         self.setMinimumWidth(350)
+        self.data =  data
         row = QHBoxLayout()
-        titleVal = BodyLabel("Valeur")
-        titleCol = BodyLabel("Colonne")
-        row.addWidget(titleVal)
-        row.addWidget(titleCol, 0, Qt.AlignRight)
+        row.addWidget(BodyLabel("Valeur"))
+        row.addWidget(BodyLabel("Colonne"), 0, Qt.AlignRight)
         self.viewLayout.addLayout(row)
         columns.insert(0, "-")
+        self.items = []
+        firstData = data[0]
         for i, item in enumerate(firstData):
             row = QHBoxLayout()
             lineEdit = LineEdit(self)
@@ -24,4 +25,15 @@ class DialogImport(MessageBoxBase):
             comboBox.setCurrentIndex(i+1 if i+1 < len(columns) else 0)
             row.addWidget(lineEdit)
             row.addWidget(comboBox)
+            self.items.append(comboBox)
             self.viewLayout.addLayout(row)
+            
+    def getData(self):
+        data = [item.currentIndex()-1 for item in self.items]
+        nData = []
+        for nItem in self.data:
+            newItem = [None] * len(data)
+            for i, n in enumerate(data):
+                newItem[n] = nItem[i]
+            nData.append(newItem)
+        return nData
