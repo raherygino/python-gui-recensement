e62ef466702ed437d716d90687a5e8a0d1a8dede
raherygino <georgino197@gmail.com>
2024-06-23 09:23:35 +0300

    Delete muliple

--- app/view/home/dialog/new_subject_dialog.py
+++ app/view/home/dialog/new_subject_dialog.py
@@ -1,6 +1,6 @@
-from PyQt5.QtWidgets import QHBoxLayout
+from PyQt5.QtWidgets import QHBoxLayout, QTableWidgetItem
 from PyQt5.QtCore import QPoint, Qt
-from PyQt5.QtGui import QCursor
+from PyQt5.QtGui import QCursor, QKeyEvent
 
 from qfluentwidgets import Dialog, Action, RoundMenu, MenuAnimationType, \
      PrimaryPushButton, PushButton, FluentIcon, SubtitleLabel, ToolButton
@@ -27,9 +27,10 @@ class NewSubjectDialog(Dialog):
         self.count = SpinBoxEditWithLabel("Nombre de matières")
         self.count.spinbox.setValue(1)
         self.count.spinbox.textChanged.connect(self.__countChange)
-        
         self.table = TableView(self)
         self.table.contextMenuEvent = lambda event: self.contextMenu(event)
+        self.table.itemClicked.connect(self.itemClicked)
+        self.table.keyPressEvent = self.keyPress
         self.table.setHorizontalHeaderLabels(["ID",  "Abréviation", "Rubrique", "Coeff"])
         self.table.setRowCount(1)
         self.table.setColumnCount(4)
@@ -50,20 +51,37 @@ class NewSubjectDialog(Dialog):
         self.buttonLayout.addWidget(self.cancelBtn)
         
         self.setFixedWidth(450)
+
+    def keyPress(self, event: QKeyEvent | None) -> None:
+        if event.key() == Qt.Key_Delete:
+            items = self.table.selectionModel().selectedRows()
+            if len(items) > 0:
+                self.deleteSubject(items)
+        
+    def itemClicked(self, item: QTableWidgetItem):
+        if item.column() == 0:
+            for i in range(self.table.columnCount()):
+                self.table.item(item.row(), i).setSelected(True)
+        
+    def itemRightClicked(self, item: QTableWidgetItem):
+        for i in range(0, self.table.columnCount()):
+            self.table.item(item.row(), i).setSelected(True)
         
     def contextMenu(self, event):
+        for item in self.table.selectedItems():
+            self.itemRightClicked(item)
         items = self.table.selectionModel().selectedRows()
-        menu = RoundMenu(parent=self)
-        menu.addAction(Action(FluentIcon.DELETE, 'Supprimer', triggered = lambda:self.deleteSubject(items)))
-        self.posCur = QCursor().pos()
-        cur_x = self.posCur.x()
-        cur_y = self.posCur.y()
-        menu.exec(QPoint(cur_x, cur_y), aniType=MenuAnimationType.FADE_IN_DROP_DOWN)
+        if len(items) > 0:
+            menu = RoundMenu(parent=self)
+            menu.addAction(Action(FluentIcon.DELETE, 'Supprimer', triggered = lambda:self.deleteSubject(items)))
+            self.posCur = QCursor().pos()
+            cur_x = self.posCur.x()
+            cur_y = self.posCur.y()
+            menu.exec(QPoint(cur_x, cur_y), aniType=MenuAnimationType.FADE_IN_DROP_DOWN)
         
     def deleteSubject(self, items):
         dialog = Dialog("Supprimer?", "Voulez vous supprimer vraiment?", self)
         dialog.setTitleBarVisible(False)
-        print(items)
         if dialog.exec():
             for index in sorted(items, key=lambda x: x.row(), reverse=True):
                 self.table.removeRow(index.row())
