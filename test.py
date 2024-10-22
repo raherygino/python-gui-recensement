30801316c5853add88771143ec5264fc95fbd750
raherygino <georgino197@gmail.com>
2024-10-22 15:13:04 +0300

    New update

--- test.py
+++
@@ -1,8 +0,0 @@
-def is_palindrome(s:str):
-    s = s.lower().strip(" ")
-    for i in range(len(s)):
-        if s[i] != s[-(i+1)]:
-            return False
-    return True
-
-print(is_palindrome("un homme planifie un canal panama"))
